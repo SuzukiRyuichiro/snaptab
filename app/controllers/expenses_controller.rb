7bd097216216b5c4fc67a85e52ba3f1951092681
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2026-06-07 16:31:29 +0900

    just showing the month

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,15 @@
 class ExpensesController < ApplicationController
   def index
-    @expenses = policy_scope(Expense).joins(:category)
+    from = Date.strptime(params[:month], "%Y-%m")
+    to = from.end_of_month
+    expense_and_category = policy_scope(Expense).joins(:category).where("spent_at between ? and ?", from, to)
+    @expenses = expense_and_category.order(spent_at: :desc)
+    @category_breakdown = expense_and_category.group("categories.display_name, categories.slug").sum(:amount)
+    # Scope only the selected month
+    # query params would have the selected month params[:month]=2026-06
+    # if not, or the format is not good, use the current date, force it
+    #
+    # Group possible months
   end
 
   def new
